124fc25fa6a477afed586df753e31200b7406295
Bryson Kruk <brysonmk1984@gmail.com>
2018-07-24 11:40:48 -0600

    added way to create account

--- index.js
+++ index.js
@@ -6,13 +6,34 @@ const bodyParser = require("body-parser");
 const pg = require('pg');
 const Sequelize = require('sequelize');
 const dbUrl = process.env.NODE_ENV === 'PRODUCTION' ? process.env.DB_URL : "postgres://admin:admin@localhost/ecoAlliesLogin";
-const sequelize = new Sequelize(dbUrl);
+const connection = new Sequelize(dbUrl);
 
-console.log('!!!!', process.env.NODE_ENV);
-sequelize
+const UserModel = require('./user');
+
+connection
   .authenticate()
   .then(() => {
     console.log('Connection has been established successfully.');
+    
+    
+
+    connection.sync().then(function(){
+        const User = UserModel(connection, Sequelize);
+        const formData = {
+            email : 'brysonmk1988@gmail.com',
+            password : 'graphic5',
+            passwd : 'graphic5',
+            publicEthKey : '0x00000000000000000'
+        };
+        if(formData.password !== formData.passwd){
+           throw('PASS MISMATCH!'); 
+        }
+        User.create({
+            email : formData.email,
+            password : formData.password,
+            publicEthKey : formData.publicEthKey,
+        })
+    });
   })
   .catch(err => {
     console.error('Unable to connect to the database:', err);
